b7c338f0bbe9a5186d96ab310967a6ddeca01832
t.sato <satoutomoaki@satoutomoakinoMacBook-Air.local>
2017-09-23 22:28:37 +0900

    サンプルで簡単なテンプレートまで

---
+++ app/assets/javascripts/components/footer.js.jsx
@@ -0,0 +1,11 @@
+var Footer = React.createClass({
+
+  render: function() {
+    return (
+      <footer className="footer">
+        <p>Copyright© みんなの気持ち All Rights Reserved.</p>
+      </footer>
+    );
+  }
+});
+
